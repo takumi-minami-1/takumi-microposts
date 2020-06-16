15f639b5df8ead1c102481a794a98620e72c6e35
minami <reclamationttt@icloud.com>
2020-06-16 10:22:52 +0000

    post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,15 @@ class ApplicationController < ActionController::Base
 
   private
 
+  private
+
   def require_user_logged_in
     unless logged_in?
       redirect_to login_url
     end
+  end
+
+  def counts(user)
+    @count_microposts = user.microposts.count
   end
 end
